aa522b5135943ab883e93e38118b5d9ce7d2b9b8
ALANJAEL <144287332+ALANJAEL@users.noreply.github.com>
2023-09-25 22:34:59 -0600

    Add files via upload

---
+++ p4_1s22.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class P4_1s22{
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		String temp;
+		int num1, num2;
+		int resultado;
+
+		System.out.println("ingresa el primer numero: ");
+		temp = teclado.nextLine();
+		num1 = Integer.parseInt(temp);
+		System.out.println("Ingresa el segundo numero: ");
+		temp = teclado.nextLine();
+		num2 = Integer.parseInt(temp);
+		resultado = num1 + num2;
+		System.out.println("La suma es: " + resultado);
+
+	}
+}
