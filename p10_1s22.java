afa464dc3050802b92f8fd40529e9ca981d791ef
ALANJAEL <144287332+ALANJAEL@users.noreply.github.com>
2023-09-25 22:37:26 -0600

    Add files via upload

---
+++ p10_1s22.java
@@ -0,0 +1,9 @@
+public class p10_1s22{
+	public static void main(String[] args) {
+		
+		if (false){
+			System.out.println("por que aparezco...");
+		}
+	   System.out.println("que paso...");
+	}
+}
