f92aceb0bcea802338c6c723ef6433bc21729120
ALANJAEL <144287332+ALANJAEL@users.noreply.github.com>
2023-09-25 22:34:28 -0600

    Add files via upload

---
+++ p3_1s22.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class p3_1s22{
+	public static void main(String[] args) {
+		String nombre;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("ingresa tu nombre:");
+		nombre = teclado.nextLine();
+		System.out.println("Bienvenido "  + nombre  +  " a la clase de programacion");
+	}
+}
